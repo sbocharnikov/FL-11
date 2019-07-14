4da8865d414de7881012b51042bf3594436f5a6d
SerhiiB <44550584+serhiib@users.noreply.github.com>
2019-07-14 21:19:47 +0300

    formatting

--- FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -4,14 +4,11 @@ function reverseNumber (num) {
         sign = -1;
         num *= -1;
     }
-
     while (num > 0) {
         revNum *= 10;
         revNum += num % 10;        
         num = Math.trunc(num / 10);
-
     }
-
     return sign * revNum;
 }
 
