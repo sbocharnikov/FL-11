31203cdef239dc3f95650a04ea695a5a9e54c582
SerhiiB <44550584+serhiib@users.noreply.github.com>
2019-09-01 23:44:49 +0300

    formatting

--- FL11_HW19/homework/src/js/draw.js
+++ FL11_HW19/homework/src/js/draw.js
@@ -1,37 +1,34 @@
 import rockImg from "../img/rock.jpg";
 import paperImg from "../img/paper.jpg";
 import scissorsImg from "../img/scissors.jpg";
-export var youImg = document.getElementById("you-img");
-export var computerImg = document.getElementById("computer-img");
-var rockImgPath = "./img/rock.jpg";
-var paperImgPath = "./img/paper.jpg";
-var scissorsImgPath = "./img/scissors.jpg";
-export var draw = function draw(yourFigure, computerFigure) {
-  switch (yourFigure) {
-    case "rock":
-      youImg.src = rockImgPath;
-      break;
+export const youImg = document.getElementById('you-img');
+export const computerImg = document.getElementById('computer-img');
+const rockImgPath = './img/rock.jpg';
+const paperImgPath = './img/paper.jpg';
+const scissorsImgPath = './img/scissors.jpg';
 
-    case "paper":
-      youImg.src = paperImgPath;
-      break;
-
-    case "scissors":
-      youImg.src = scissorsImgPath;
-      break;
-  }
-
-  switch (computerFigure) {
-    case "rock":
-      computerImg.src = rockImgPath;
-      break;
-
-    case "paper":
-      computerImg.src = paperImgPath;
-      break;
-
-    case "scissors":
-      computerImg.src = scissorsImgPath;
-      break;
-  }
-};
+export const draw = (yourFigure, computerFigure) => {
+    switch (yourFigure) {
+      case 'rock':
+        youImg.src = rockImgPath;
+        break;
+      case 'paper':
+        youImg.src = paperImgPath;
+        break;
+      case 'scissors':
+        youImg.src = scissorsImgPath;
+        break;
+    }
+  
+    switch (computerFigure) {
+      case 'rock':
+        computerImg.src = rockImgPath;
+        break;
+      case 'paper':
+        computerImg.src = paperImgPath;
+        break;
+      case 'scissors':
+        computerImg.src = scissorsImgPath;
+        break;
+    }
+  };
--- FL11_HW19/homework/src/js/index.js
+++ FL11_HW19/homework/src/js/index.js
@@ -1,102 +1,88 @@
 import "../less/styles.less";
 import { draw, youImg, computerImg } from "./draw.js";
-var buttons = document.getElementsByTagName("button");
-var rockBtn = buttons[0];
-var paperBtn = buttons[1];
-var scissorsBtn = buttons[2];
-var resetBtn = buttons[3];
-var dashImgPath = "./img/dash.jpg";
-var resultArea = document.getElementById("result-area");
-var finalResultArea = document.getElementById("final-result-area");
-var roundCount = 1;
-var yourWins = 0;
-var computerWins = 0;
 
-rockBtn.onclick = function() {
-  model("rock");
+const buttons = document.getElementsByTagName('button');
+const rockBtn = buttons[0];
+const paperBtn = buttons[1];
+const scissorsBtn = buttons[2];
+const resetBtn = buttons[3];
+const dashImgPath = './img/dash.jpg';
+const resultArea = document.getElementById('result-area');
+const finalResultArea = document.getElementById('final-result-area');
+let roundCount = 1;
+let yourWins = 0;
+let computerWins = 0;
+
+rockBtn.onclick = () => {
+  model('rock');
 };
 
-paperBtn.onclick = function() {
-  model("paper");
+paperBtn.onclick = () => {
+  model('paper');
 };
 
-scissorsBtn.onclick = function() {
-  model("scissors");
+scissorsBtn.onclick = () => {
+  model('scissors');
 };
 
-var model = function model(yourFigure) {
-  var figures = ["rock", "paper", "scissors"];
-  var random = Math.floor(Math.random() * 3);
-  var computerFigure = figures[random];
+const model = yourFigure => {
+  const figures = ['rock', 'paper', 'scissors'];
+  const random = Math.floor(Math.random() * 3);
+  const computerFigure = figures[random];
   draw(yourFigure, computerFigure);
 
   if (roundCount <= 3) {
     if (
-      (yourFigure === "rock" && computerFigure === "scissors") ||
-      (yourFigure === "paper" && computerFigure === "rock") ||
-      (yourFigure === "scissors" && computerFigure === "paper")
+      (yourFigure === 'rock' && computerFigure === 'scissors') ||
+      (yourFigure === 'paper' && computerFigure === 'rock') ||
+      (yourFigure === 'scissors' && computerFigure === 'paper')
     ) {
       yourWins++;
-      resultArea.innerHTML = "Round "
-        .concat(roundCount, ", ")
-        .concat(yourFigure, " vs ")
-        .concat(computerFigure, ", You've WON!");
+      resultArea.innerHTML = `Round ${roundCount}, ${yourFigure} vs ${computerFigure}, You've WON!`;
     }
-
     if (
-      (yourFigure === "rock" && computerFigure === "paper") ||
-      (yourFigure === "paper" && computerFigure === "scissors") ||
-      (yourFigure === "scissors" && computerFigure === "rock")
+      (yourFigure === 'rock' && computerFigure === 'paper') ||
+      (yourFigure === 'paper' && computerFigure === 'scissors') ||
+      (yourFigure === 'scissors' && computerFigure === 'rock')
     ) {
       computerWins++;
-      resultArea.innerHTML = "Round "
-        .concat(roundCount, ", ")
-        .concat(yourFigure, " vs ")
-        .concat(computerFigure, ", You've LOST!");
+      resultArea.innerHTML = `Round ${roundCount}, ${yourFigure} vs ${computerFigure}, You've LOST!`;
     }
-
     if (
-      (yourFigure === "rock" && computerFigure === "rock") ||
-      (yourFigure === "paper" && computerFigure === "paper") ||
-      (yourFigure === "scissors" && computerFigure === "scissors")
+      (yourFigure === 'rock' && computerFigure === 'rock') ||
+      (yourFigure === 'paper' && computerFigure === 'paper') ||
+      (yourFigure === 'scissors' && computerFigure === 'scissors')
     ) {
-      resultArea.innerHTML = "Round "
-        .concat(roundCount, ", ")
-        .concat(yourFigure, " vs ")
-        .concat(computerFigure, ", it's a draw!");
+      resultArea.innerHTML = `Round ${roundCount}, ${yourFigure} vs ${computerFigure}, it's a draw!`;
     }
-
     roundCount++;
   }
 
   if (roundCount > 3) {
     if (yourWins > computerWins) {
-      finalResultArea.innerHTML = "Final winner is you!";
+      finalResultArea.innerHTML = `Final winner is you!`;
     }
-
     if (yourWins < computerWins) {
-      finalResultArea.innerHTML = "Final winner is computer!";
+      finalResultArea.innerHTML = `Final winner is computer!`;
     }
-
     if (yourWins === computerWins) {
-      finalResultArea.innerHTML = "Final result is draw";
+      finalResultArea.innerHTML = `Final result is draw`;
     }
-
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
   }
 };
 
-resetBtn.onclick = function() {
+resetBtn.onclick = () => {
   youImg.src = dashImgPath;
   computerImg.src = dashImgPath;
-  rockBtn.removeAttribute("disabled");
-  paperBtn.removeAttribute("disabled");
-  scissorsBtn.removeAttribute("disabled");
-  resultArea.innerHTML = "";
-  finalResultArea.innerHTML = "";
+  rockBtn.removeAttribute('disabled');
+  paperBtn.removeAttribute('disabled');
+  scissorsBtn.removeAttribute('disabled');
+  resultArea.innerHTML = '';
+  finalResultArea.innerHTML = '';
   roundCount = 1;
   yourWins = 0;
-  computerWins = 0;
+  computerWins = 0;  
 };
